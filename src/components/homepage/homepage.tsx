1334844b60c76d67346ad5ed626afb95ca42efc5
mridul2620 <mridulsriv26@gmail.com>
2025-09-11 10:18:42 +0100

    bug fix

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,7 +1,7 @@
 // src/components/homepage/homepageContent.tsx
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./homepage.css"; // Import the CSS file
+import "../homepage/homepage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"; 
 import { useRouter } from 'next/navigation'; 
