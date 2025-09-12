bbd806d22cad8ee3997412b7c0126765af0496ee
mridul2620 <mridulsriv26@gmail.com>
2025-09-12 16:39:51 +0100

    api

--- src/components/login/login-page.tsx
+++ src/components/login/login-page.tsx
@@ -67,7 +67,7 @@ const LoginPage: React.FC = () => {
         const result = await response.json();
         console.log(result);
         localStorage.setItem("loginResponse", JSON.stringify(result));
-        window.location.replace('https://publication-portal-be.onrender.com/home-page');
+        window.location.replace('https://publication-portal-fe.vercel.app/home-page');
       } else if (response.status === 401) {
         setErrorMessage("Invalid credentials. Please try again.");
       } else {
