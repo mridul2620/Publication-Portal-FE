b5f953129d5b926461078a114fe2e687558a4288
Mridul srivastava <87673288+mridul2620@users.noreply.github.com>
2025-09-12 15:17:10 +0100

    Update page.tsx

--- app/circuit-page/page.tsx
+++ app/circuit-page/page.tsx
@@ -1,13 +1,13 @@
-
-"use client";
 import CircuitPageContent from "@/src/components/circuitpage/circuitPage";
 import React from "react";
+import { Suspense } from 'react';
+
 
 const CircuitPage = () => {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <CircuitPageContent />
-    </div>
+    </Suspense>
   );
 };
 
